f9a92f7aae4cb3269ac2d0c48603b8536bb39743
dscheirer <dscheirer@perforce.com>
2017-08-30 17:13:29 -0700

    filling in the rest of the codes (brightness, blink, etc.)

--- i2c/i2c.go
+++ i2c/i2c.go
@@ -5,28 +5,49 @@ import "syscall"
 import "fmt"
 
 type I2C struct {
-	fd *os.File
+	fd 			*os.File
 	address uint8
+	fd_sim	bool
 }
 
 const (
 	I2C_SLAVE = 0x0703
 )
 
+func logWrite(buf []uint8) error {
+	fmt.Printf("Write : ")
+	for i:=0;i<len(buf);i++ {
+		fmt.Printf("%02x ", buf[i])
+	}
+	fmt.Printf("\n")
+	return nil
+}
+
+func logMsg(msg string) error {
+	fmt.Println(msg)
+	return nil
+}
+
 // open a connection to the i2c device
-func Open(address uint8, bus int) (*I2C, error) {
-	f, err := os.OpenFile(fmt.Sprintf("/dev/i2c-%d", bus), os.O_RDWR, 0600)
-	if err != nil {
-		return nil, err
+func Open(address uint8, bus int, simulated bool) (*I2C, error) {
+	if !simulated {
+		f, err := os.OpenFile(fmt.Sprintf("/dev/i2c-%d", bus), os.O_RDWR, 0600)
+		if err != nil {
+			return nil, err
+		}
+		if err := ioctl(f.Fd(), I2C_SLAVE, uintptr(address)); err != nil {
+			return nil, err
+		}
+		this := &I2C{fd: f, address: address, fd_sim: false}
+		return this, nil
+	} else {
+		this := &I2C{fd_sim: true, address: address, fd: nil}
+		return this, nil
 	}
-	if err := ioctl(f.Fd(), I2C_SLAVE, uintptr(address)); err != nil {
-		return nil, err
-	}
-	this := &I2C{fd: f, address: address}
-	return this, nil
 }
 
 func (this *I2C) Close() error {
+	if this.fd_sim { return logMsg(fmt.Sprintf("Close: %d", this.address)) }
 	return this.fd.Close()
 }
 
@@ -38,7 +59,12 @@ func (this *I2C) WriteByte(single byte) (int, error) {
 	if err := select_line(this); err != nil {
 		return 0, err
 	}
-	return this.fd.Write(buf[:])
+
+	if this.fd_sim {
+		return 0, logWrite(buf[:])
+	} else {
+		return this.fd.Write(buf[:])
+	}
 }
 
 func (this *I2C) Write(buf []uint8) (int, error) {
@@ -46,11 +72,19 @@ func (this *I2C) Write(buf []uint8) (int, error) {
 	if err := select_line(this); err != nil {
 		return 0, err
 	}
-	return this.fd.Write(buf)
+	if this.fd_sim {
+		return 0, logWrite(buf)
+	} else {
+		return this.fd.Write(buf)
+	}
 }
 
 func select_line(this *I2C) error {
-	return ioctl(this.fd.Fd(), I2C_SLAVE, uintptr(this.address))
+	if this.fd_sim {
+		return logMsg(fmt.Sprintf("ioctl: I2C_SLAVE @ 0x%02x", this.address))
+	} else {
+		return ioctl(this.fd.Fd(), I2C_SLAVE, uintptr(this.address))
+	}
 }
 
 func ioctl(fd, cmd, arg uintptr) error {
--- sevenseg_backpack/sevenseg_backpack.go
+++ sevenseg_backpack/sevenseg_backpack.go
@@ -18,11 +18,6 @@ const i2c_DISPLAY_CMD = 0x80
 const i2c_DISPLAY_ON  = 0x81
 const i2c_DISPLAY_OFF = 0x80
 
-const i2c_BLINK_OFF = 0
-const i2c_BLINK_2HZ = 1
-const i2c_BLINK_1HZ = 2
-const i2c_BLINK_HALFHZ = 3
-
 // 0x0 -> 0xF brightness levels
 const i2c_BRIGHTNESS_CMD  = 0xE0
 const i2c_BRIGHTNESS_MAX  = 0xEF
@@ -31,6 +26,12 @@ const i2c_BRIGHTNESS_HALF = 0xE7
 
 // colon is just one bit at position 5 (+1 for address, position 2 * 2 for nil bytes)
 const i2c_COLON_POS       = 1 + 2*2
+
+// export blink positions
+const BLINK_OFF = 0
+const BLINK_2HZ = 1
+const BLINK_1HZ = 2
+const BLINK_HALFHZ = 3
 
 // positions of segments
 const LED_TOP             = 0
@@ -93,6 +94,8 @@ type Sevenseg struct {
 	display [displaySize]uint8
 	i2c_dev *i2c.I2C
     refresh bool
+    inverted bool
+    blink byte
 }
 
 func getClearDisplay() [displaySize]uint8 {
@@ -103,13 +106,18 @@ func getClearDisplay() [displaySize]uint8 {
     return display
 }
 
-func Open(address uint8, bus int) (*Sevenseg, error) {
-	i2c_dev, err := i2c.Open(address, bus)
+func Open(address uint8, bus int, simulated bool) (*Sevenseg, error) {
+	i2c_dev, err := i2c.Open(address, bus, simulated)
 	if err != nil {
 		return nil, err
 	}
     // create "this" with the FD and refresh on by default
-	this := &Sevenseg{i2c_dev: i2c_dev, refresh: true, display: getClearDisplay()}
+	this := &Sevenseg{
+        i2c_dev: i2c_dev,
+        refresh: true,
+        inverted: false,
+        blink: BLINK_OFF,
+        display: getClearDisplay() }
     // turn on the oscillator, set default brightness
     this.i2c_dev.WriteByte(i2c_OSC_ON)
     this.i2c_dev.WriteByte(i2c_BRIGHTNESS_MAX)
@@ -118,8 +126,13 @@ func Open(address uint8, bus int) (*Sevenseg, error) {
 	return this, nil
 }
 
+func (this *Sevenseg) SetInverted(inverted bool) {
+    this.inverted = inverted
+}
+
 func (this *Sevenseg) DisplayOn(on bool) error {
-    var val byte = i2c_DISPLAY_ON
+    // blink rate is bits 2 and 1 of the display command
+    var val byte = i2c_DISPLAY_ON | (this.blink << 1)
     if !on { val = i2c_DISPLAY_OFF }
     _, err := this.i2c_dev.WriteByte(val)
     return err
@@ -135,9 +148,105 @@ func (this *Sevenseg) RefreshOn(on bool) error {
     return this.refresh_display()
 }
 
+func (this *Sevenseg) dumpDisplay() {
+    //  -     -      -     -
+    // | |   | |  . | |   | |
+    //  -     -      -     -
+    // | |   | |  . | |   | |
+    //  -  .  -  .   -  .  -  .
+
+    // go one row at a time
+    // TOP
+    var i byte;
+    for i=0;i<4;i++ {
+        if i == 2 {
+            fmt.Printf(" ")
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_TOP) != 0 {
+            fmt.Printf("  -   ")
+        } else {
+            fmt.Printf("      ")
+        }
+    }
+    // TOPM
+    fmt.Println()
+    for i=0;i<4;i++ {
+        if i == 2 {
+            if this.display[i2c_COLON_POS] != 0 {
+                fmt.Printf(".")
+            } else {
+                fmt.Printf(" ")
+            }
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_TOPL) != 0  {
+            fmt.Printf(" |")
+        } else {
+            fmt.Printf("  ")
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_TOPR) != 0  {
+            fmt.Printf(" |  ")
+        } else {
+            fmt.Printf("    ")
+        }
+    }
+    // MID
+    fmt.Println()
+    for i=0;i<4;i++ {
+        if i == 2 {
+            fmt.Printf(" ")
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_MID) != 0  {
+            fmt.Printf("  -   ")
+        } else {
+            fmt.Printf("      ")
+        }
+    }
+    // BOTM
+    fmt.Println()
+    for i=0;i<4;i++ {
+        if i == 2 {
+            if this.display[i2c_COLON_POS] != 0  {
+                fmt.Printf(".")
+            } else {
+                fmt.Printf(" ")
+            }
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_BOTL) != 0  {
+            fmt.Printf(" |")
+        } else {
+            fmt.Printf("  ")
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_BOTR) != 0  {
+            fmt.Printf(" |  ")
+        } else {
+            fmt.Printf("    ")
+        }
+    }
+    // BOT
+    fmt.Println()
+    for i=0;i<4;i++ {
+        if i == 2 {
+            fmt.Printf(" ")
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_BOT) != 0  {
+            fmt.Printf("  -  ")
+        } else {
+            fmt.Printf("     ")
+        }
+        if this.display[getDisplayPos(i)] & (1 << LED_DECIMAL) != 0  {
+            fmt.Printf(".")
+        } else {
+            fmt.Printf(" ")
+        }
+    }
+    fmt.Println()
+}
+
 func (this *Sevenseg) refresh_display() error {
     if !this.refresh { return nil }
     // display has the address 0 embedded in it
+    // for debugging, dump out twhat we think we're putting on the display
+    this.dumpDisplay()
 	_, err := this.i2c_dev.Write(this.display[:])
     return err
 }
@@ -176,8 +285,17 @@ func (this *Sevenseg) SegmentOn(position byte, segment byte, on bool) error {
 
 func (this *Sevenseg) getMask(char uint8, decimalOn bool) (byte, error) {
     // TODO: inverse support
-    val, ok := digitValues[char]
+    var val uint8
+    var ok bool
+    if !this.inverted {
+        val, ok = digitValues[char]
+    } else {
+        val, ok = inverseDigitValues[char]
+    }
     if !ok { return 0, errors.New(fmt.Sprintf("Bad value: %d", char))}
+    if decimalOn {
+        val |= (1 << LED_DECIMAL)
+    }
     return val, nil
 }
 
@@ -188,11 +306,18 @@ func (this *Sevenseg) PrintColon(msg string) error {
         return errors.New("Too many colons: " + msg)
     }
     display := getClearDisplay()
-
+    // first do parts[0]
     // not DRY
-    var displayPos byte = 1
+    var displayPos = 1
+    var inc = -1
+    var bound = -1
+    if this.inverted {
+        displayPos = 2
+        inc = +1
+        bound = 4
+    }
     var i = len(parts[0])-1
-    for ;i>=0 && displayPos>=0;i-- {
+    for ;i>=0 && displayPos!=bound;i-- {
         // map parts[0][i] to a character or dot
         dotOn := false
         if parts[0][i]=='.' && i > 0 {
@@ -202,8 +327,8 @@ func (this *Sevenseg) PrintColon(msg string) error {
         // is it in our table?
         mask, err := this.getMask(parts[0][i], dotOn)
         if err != nil { return err }
-        display[getDisplayPos(displayPos)] = mask
-        displayPos--
+        display[getDisplayPos(byte(displayPos))] = mask
+        displayPos+=inc
     }
     // did we get it all?
     if i != -1 { return errors.New("Too many characters: " + msg) }
@@ -211,7 +336,14 @@ func (this *Sevenseg) PrintColon(msg string) error {
     // now the other half
     // not DRY
     displayPos = 2
-    for i=0;i<len(parts[1]) && displayPos<byte(len(display));i-- {
+    inc = +1
+    bound = 4
+    if this.inverted {
+        displayPos = 1
+        inc = -1
+        bound = -1
+    }
+    for i=0;i<len(parts[1]) && displayPos!=bound;i++ {
         // map parts[1][i] to a character or dot
         // with a dot?
         dotOn := false
@@ -221,8 +353,11 @@ func (this *Sevenseg) PrintColon(msg string) error {
         // is it in our table?
         mask, err := this.getMask(parts[1][i], dotOn)
         if err != nil { return err }
-        display[getDisplayPos(displayPos)] = mask
-        displayPos++
+        display[getDisplayPos(byte(displayPos))] = mask
+        displayPos+=inc
+        if dotOn {
+            i++;
+        }
     }
     // did we get it all?
     if i != len(parts[1]) { return errors.New("Too many characters: " + msg) }
@@ -239,10 +374,18 @@ func (this *Sevenseg) Print(msg string) error {
     if strings.Contains(msg, ":") { return this.PrintColon(msg) }
     // string can only contain chars in our map and decimals
     // assume it's right justified (reverse walk)
+    // TODO: adjust for inverse using displayPos and direction
     display := getClearDisplay()
-    var displayPos byte = 3;
+    var displayPos = 3
+    var inc = -1
+    var bound = -1
+    if this.inverted {
+        displayPos = 0
+        inc = +1
+        bound = 4
+    }
     var i = len(msg)-1
-    for ;i>=0 && displayPos>=0;i-- {
+    for ;i>=0 && displayPos!=bound;i-- {
         // map msg[i] to a character or dot
         dotOn := false
         if msg[i]=='.' {
@@ -252,8 +395,8 @@ func (this *Sevenseg) Print(msg string) error {
         // is it in our table?
         mask, err := this.getMask(msg[i], dotOn)
         if err != nil { return err }
-        display[getDisplayPos(displayPos)] = mask
-        displayPos--
+        display[getDisplayPos(byte(displayPos))] = mask
+        displayPos+=inc
     }
     // did we get it all?
     if i != -1 { return errors.New("Too many characters: " + msg) }
@@ -262,15 +405,22 @@ func (this *Sevenseg) Print(msg string) error {
     return this.refresh_display()
 }
 
-func (this *Sevenseg) PrintOffset(msg string, position byte) error {
+func (this *Sevenseg) PrintOffset(msg string, position int) error {
     if strings.Contains(msg, ":") { return this.PrintColon(msg) }
     // string can only contain chars in our map and decimals
     // assume it's left justified (forward walk) with an offset
     display := getClearDisplay()
     var displayPos = position;
+    var inc = +1
+    var bound = 4
+    if this.inverted {
+        displayPos = 3-position
+        inc = -1
+        bound = -1
+    }
 
     var i = 0
-    for ;i<len(msg) && displayPos<byte(len(display));i-- {
+    for ;i<len(msg) && displayPos!=bound;i++ {
         // map msg[i] to a character or dot
         // with a dot?
         dotOn := false
@@ -280,12 +430,25 @@ func (this *Sevenseg) PrintOffset(msg string, position byte) error {
         // is it in our table?
         mask, err := this.getMask(msg[i], dotOn)
         if err != nil { return err }
-        display[getDisplayPos(displayPos)] = mask
-        displayPos++
+        display[getDisplayPos(byte(displayPos))] = mask
+        displayPos+=inc
     }
     // did we get it all?
     if i != len(msg) { return errors.New("Too many characters: " + msg) }
     // set the display
     this.display = display
     return this.refresh_display()
+}
+
+func (this *Sevenseg) SetBlinkRate(rate uint8) error {
+    if rate > 3 { return errors.New(fmt.Sprintf("Bad blink rate: ", rate)) }
+    this.blink = rate
+    // one assumes you want the display on now?
+    return this.DisplayOn(true)
+}
+
+func (this *Sevenseg) SetBrightness(level uint8) error {
+    if level > 15 { return errors.New(fmt.Sprintf("Bad brightness level: %d", level)) }
+    _, err := this.i2c_dev.WriteByte(i2c_BRIGHTNESS_CMD | level)
+    return err
 }
