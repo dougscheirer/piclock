928ba3a8801efb7c140944cf8b688a71b15f8091
dscheirer <doug@dscheirer.com>
2020-03-03 08:38:26 -0800

    reload button working

--- pireload/reload.sh
+++ pireload/reload.sh
@@ -6,9 +6,19 @@ die()
   exit 1
 }
 
+
+GO=/home/pi/gvm/go
+
+echo "Version is $($GO version)"
+
 # do a git pull, a go test/build, and start the service
-git pull || die "failed to perform git pull"
-go test || die "go tests failed!"
-go build || die "go build failed!"
+su pi -c "git pull" || die "failed to perform git pull"
+su pi -c "$GO test -v" >> ./test-output.txt 2>&1
+
+if [ ! $? -eq 0 ] ; then 
+  die "go tests failed!"
+fi
+
+su pi -c "$GO build" || die "go build failed!"
 
 systemctl restart piclock.service || die "failed to restart service"
